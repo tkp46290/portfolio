ab010f69e45e08e65df4855815eaa3bc9b0df4c9
tkp46290 <tarunkumarpal123@gmail.com>
2023-05-13 14:03:48 +0530

    photo

--- src/About.jsx
+++ src/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import aboutImg from "../assets/images/project_person.PNG";
+import aboutImg from "../assets/images/profile.jpg";
 const About = () => {
   // const info = [
   //   { text: "Years experience", count: "04" },
@@ -9,16 +9,21 @@ const About = () => {
   return (
     <section id="about" className="py-10 text-white">
       <div className="text-center mt-8">
-        <h3 className="text-4xl text-[pink] font-semibold">
-          About Me
-        </h3>
+        <h3 className="text-4xl text-[pink] font-semibold">About Me</h3>
         <p className="text-gray-400 my-3 text-lg">Who I am</p>
         <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
           <div className="p-2">
             <div className="text-gray-300 my-3">
               <p className="text-justify leading-7 w-11/12 mx-auto">
-              m a recent graduate with a degree in Mechanical Engineering and  but I have a passion for web development. I have hands on  in ReactJS and I have experience building responsive, user-friendly web applications using this framework.
-I gained hands-on experience in web development through several projects . I'm proficient in HTML, CSS, JavaScript, and I have experience working with various front-end technologies such as ReactJS, Redux, and Bootstrap. I'm also familiar with back-end development  such as Node.js and Express.js.
+                m a recent graduate with a degree in Mechanical Engineering and
+                but I have a passion for web development. I have hands on in
+                ReactJS and I have experience building responsive, user-friendly
+                web applications using this framework. I gained hands-on
+                experience in web development through several projects . I'm
+                proficient in HTML, CSS, JavaScript, and I have experience
+                working with various front-end technologies such as ReactJS,
+                Redux, and Bootstrap. I'm also familiar with back-end
+                development such as Node.js and Express.js.
               </p>
               {/* <div className="flex mt-10 items-center gap-7">
                 {info.map((content) => (
